3b6653e7d66bc011bac1c09aeb5a2e42e77361fa
rampanellijader <jader.rampa@gmail.com>
2020-04-04 00:29:48 -0300

    commit inicial dos arquivos do projeto

---
+++ src/test/java/com/br/curso/api/CursoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.curso.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
